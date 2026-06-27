608e5aa6f9f321ab17c3daa35cc9026166bbb895
alex202-sys <aleksandr.ogloblin.job@gmail.com>
2026-06-27 14:39:14 +0200

    docs. urls.py cleaned up

--- Backend/kanban_app/api/urls.py
+++ Backend/kanban_app/api/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path, include
-
-# from rest_framework import routers
 from rest_framework.routers import DefaultRouter
 from .views import (
     OfferViewSet,
@@ -11,15 +9,6 @@ from .views import (
     BaseInfoViewSet,
     ReviewViewSet,
 )
-
-# from kanban_app.api.views import BoardListView, TasksView, EmailCheckView
-
-# router = routers.SimpleRouter()
-# router.register(r'boards', BoardListView)
-# router.register(r'tasks', TasksView, basename='tasks')
-# router.register(r'email-check', EmailCheckView, basename='email-check')
-# urlpatterns = [path("", include(router.urls))]
-
 
 router = DefaultRouter()
 router.register(r"offers", OfferViewSet, basename="offer")  # "offer"
