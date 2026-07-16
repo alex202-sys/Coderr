5dff47bf8e3c4f8413b763f23b85261e8bc014d7
alex202-sys <aleksandr.ogloblin.job@gmail.com>
2026-07-16 08:49:32 +0000

    made app ready to server with static files setup

--- Backend/core/settings_bac.py
+++ Backend/core/settings_bac.py
@@ -27,8 +27,7 @@ if not SECRET_KEY:
 DEBUG = True
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['liuhequan.org', 'www.liuhequan.org', '35.242.227.214', 'localhost', '127.0.0.1']
-
+ALLOWED_HOSTS = ['35.242.227.214', 'localhost', '127.0.0.1']
 # CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:5500", "http://localhost:5500"]
 
 # Application definition
@@ -124,7 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = '/home/aleksandr_ogloblin_job/projects/Coderr/Backend/static/'
 
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
