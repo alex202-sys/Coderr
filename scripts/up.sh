9301c71c140fae2c8e2e18b42b4458d153e917a5
alex202-sys <aleksandr.ogloblin.job@gmail.com>
2026-07-18 12:32:07 +0000

    fix: The problem is that the DNS server doesn't accept the SSH key during forwarding. The script was been fixed

--- scripts/up.sh
+++ scripts/up.sh
@@ -3,4 +3,5 @@ cd ~/projects/Coderr/ || exit
 git add .
 git commit -m "$*"
 git push
-ssh -a aleksandr_ogloblin_job@liuhequan.org "cd /home/aleksandr_ogloblin_job/projects/Coderr/ && git pull" 
+ssh -o ForwardAgent=yes aleksandr_ogloblin_job@liuhequan.org "cd /home/aleksandr_ogloblin_job/projects/Coderr/ && git pull"
+ 
