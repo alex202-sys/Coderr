1636e57fd3d96c533eaa49d2a75ff64a4db42adb
alex202-sys <aleksandr.ogloblin.job@gmail.com>
2026-07-18 13:01:54 +0000

    fix: Script Coderr/scripts/up.sh for lokale start in the server enviroment was adapted

--- scripts/up.sh
+++ scripts/up.sh
@@ -3,5 +3,9 @@ cd ~/projects/Coderr/ || exit
 git add .
 git commit -m "$*"
 git push
-ssh -o ForwardAgent=yes aleksandr_ogloblin_job@liuhequan.org "cd /home/aleksandr_ogloblin_job/projects/Coderr/ && git pull"
+
+# ssh -o ForwardAgent=yes aleksandr_ogloblin_job@liuhequan.org "cd /home/aleksandr_ogloblin_job/projects/Coderr/ && git pull"
+
+# No SSH connection required to start directly on VS! Run the pull directly locally::
+cd /home/aleksandr_ogloblin_job/projects/Coderr/ && git pull
  
