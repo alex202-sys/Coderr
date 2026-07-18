1539ac14dbbeb21977663101d45e1f47deadd6b8
alex202-sys <aleksandr.ogloblin.job@gmail.com>
2026-07-18 13:13:08 +0000

    fix: Ownership rights for his own project directory were transferred back from root to the regular user.

--- scripts/up.sh
+++ scripts/up.sh
@@ -6,6 +6,6 @@ git push
 
 # ssh -o ForwardAgent=yes aleksandr_ogloblin_job@liuhequan.org "cd /home/aleksandr_ogloblin_job/projects/Coderr/ && git pull"
 
-# No SSH connection required to start directly on VS! Run the pull directly locally::
+# No SSH connection required to start directly on VS! Run the pull directly locally:
 cd /home/aleksandr_ogloblin_job/projects/Coderr/ && git pull
  
